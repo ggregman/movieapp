c6db99dc7312e86d0cb76073ed2dd1b819dcce81
Gregorio Manahan <gregorio.manahan@gmail.com>
2022-05-25 12:09:00 +0200

    fetch serie

--- js/modulo-api.js
+++ js/modulo-api.js
@@ -3,13 +3,24 @@ export const pippo = "https://www.omdbapi.com/?apikey=f6e1faa9&s=terminator";
 export const somma = (param1,param2) => param1 + param2;
 export const divisione = (param1,param2) => param1 / param2;
 
-const URL_LIST_AVENGERS = "https://www.omdbapi.com/?apikey=f6e1faa9&s=avengers";
-
+//const URL_LIST_AVENGERS = "https://www.omdbapi.com/?apikey=f6e1faa9&s=avengers";
+const URL_LIST_AVENGERS_SERIES = "https://www.omdbapi.com/?apikey=f6e1faa9&s=avengers&type=series";
+/*
 export const listAvengers = () => {
     fetch(URL_LIST_AVENGERS)
     .then((response) => response.json())
     .then((results) => {
         const movies = results.Search;
         console.log(movies);
+    });
+*/
+
+export const listAvengersSeries = () => {
+    fetch(URL_LIST_AVENGERS_SERIES)
+    .then((response1) => response1.json())
+    .then((results1) => {
+        const movies1 = results1.Search;
+        console.log(movies1);
     }) 
+
 };

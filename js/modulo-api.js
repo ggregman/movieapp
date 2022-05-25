832f536e8f5a1835cb18c9e419d4483867cbe6ee
Gregorio Manahan <gregorio.manahan@gmail.com>
2022-05-25 12:17:48 +0200

    correzione sintassi fetch series

--- js/modulo-api.js
+++ js/modulo-api.js
@@ -3,24 +3,30 @@ export const pippo = "https://www.omdbapi.com/?apikey=f6e1faa9&s=terminator";
 export const somma = (param1,param2) => param1 + param2;
 export const divisione = (param1,param2) => param1 / param2;
 
-//const URL_LIST_AVENGERS = "https://www.omdbapi.com/?apikey=f6e1faa9&s=avengers";
-const URL_LIST_AVENGERS_SERIES = "https://www.omdbapi.com/?apikey=f6e1faa9&s=avengers&type=series";
-/*
+const BASE_URL = "https://www.omdbapi.com/?apikey=f6e1faa9&";
+
+const URL_LIST_AVENGERS = BASE_URL + "s=avengers";
+const URL_LIST_AVENGERS_SERIES =  BASE_URL + "s=avengers&type=series";
+
 export const listAvengers = () => {
     fetch(URL_LIST_AVENGERS)
     .then((response) => response.json())
     .then((results) => {
         const movies = results.Search;
         console.log(movies);
-    });
-*/
+    })
+};
+
 
 export const listAvengersSeries = () => {
     fetch(URL_LIST_AVENGERS_SERIES)
-    .then((response1) => response1.json())
-    .then((results1) => {
-        const movies1 = results1.Search;
-        console.log(movies1);
-    }) 
-
+    .then((response) => response.json())
+    .then((results) => {
+        const movies = results.Search;
+        console.log(movies);
+    })
 };
+/*
+export const apiList = () => {
+
+}*/

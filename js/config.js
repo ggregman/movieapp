a4e0938d818da7e5e6e640e2c8480059ea876423
Gregorio Manahan <gregorio.manahan@gmail.com>
2022-05-25 14:10:32 +0200

    correzione errore import

---
+++ js/config.js
@@ -0,0 +1,9 @@
+export const API_KEY = "f6e1faa9";
+export const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&`;
+
+export const CONTENT_TYPE = {
+    MOVIE: "movie",
+    SERIES: "series",
+    EPISODE: "episode",
+    GAME: "game"
+}
--- js/modulo-api.js
+++ js/modulo-api.js
@@ -1,32 +1,30 @@
-export const pippo = "https://www.omdbapi.com/?apikey=f6e1faa9&s=terminator";
+/*export const pippo = "https://www.omdbapi.com/?apikey=f6e1faa9&s=terminator";
 
 export const somma = (param1,param2) => param1 + param2;
-export const divisione = (param1,param2) => param1 / param2;
+export const divisione = (param1,param2) => param1 / param2;*/
 
-const BASE_URL = "https://www.omdbapi.com/?apikey=f6e1faa9&";
-
-const URL_LIST_AVENGERS = BASE_URL + "s=avengers";
-const URL_LIST_AVENGERS_SERIES =  BASE_URL + "s=avengers&type=series";
-
+/*
+export const URL_LIST_AVENGERS = "s=avengers";
+export const URL_LIST_AVENGERS_SERIES = "s=avengers&type=series";
+*/
+/*
 export const listAvengers = () => {
-    fetch(URL_LIST_AVENGERS)
-    .then((response) => response.json())
-    .then((results) => {
-        const movies = results.Search;
-        console.log(movies);
-    })
-};
-
+    apiList(URL_LIST_AVENGERS)
+ };
 
 export const listAvengersSeries = () => {
-    fetch(URL_LIST_AVENGERS_SERIES)
-    .then((response) => response.json())
-    .then((results) => {
-        const movies = results.Search;
-        console.log(movies);
-    })
+   apiList(URL_LIST_AVENGERS_SERIES)
 };
-/*
-export const apiList = () => {
+*/
 
-}*/
+import {BASE_URL} from "./config.js";
+
+export const apiList = (s, type) => {
+    const url = BASE_URL + `s=${s}&type=${type}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((results) => {
+            const items = results.Search;
+            console.log(items);
+        })
+};
